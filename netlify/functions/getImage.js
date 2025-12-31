7032a52207e8ed5b5bdafe6970fbfeea51754af2
Yu1tata <chyi24020@g.nihon-u.ac.jp>
2026-01-01 01:54:19 +0900

    更新版

--- netlify/functions/getImage.js
+++ netlify/functions/getImage.js
@@ -2,17 +2,13 @@ import { getStore } from "@netlify/blobs";
 
 export default async (req) => {
   const id = new URL(req.url).searchParams.get("id");
+
   const store = getStore("images");
 
-  const blob = await store.get(id);
+  const base64 = await store.get(id);
 
-  if (!blob) {
-    return new Response("Not found", { status: 404 });
-  }
-
-  return new Response(blob, {
-    headers: {
-      "Content-Type": "image/gif"
-    }
-  });
+  return new Response(
+    Buffer.from(base64, "base64"),
+    { headers: { "Content-Type": "image/gif" } }
+  );
 };
